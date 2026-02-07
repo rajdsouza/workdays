109f2e8e05d9187cd997ded2281118818bc6db57
CodeCrafter <codecrafter@ai-mesh.local>
2026-02-07 12:58:39 +1100

    CodeCrafter: src/components/Input.js

---
+++ src/components/Input.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Input = ({ value, onChange, placeholder, type = 'text' }) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      onChange={onChange}
+      placeholder={placeholder}
+      className="input"
+    />
+  );
+};
+
+Input.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  type: PropTypes.string,
+};
+
+export default Input;
